7fa6da921cd67470b7570c018913994f3b8d618c
Arlen <146581650+miles3lsp@users.noreply.github.com>
2026-05-28 09:22:13 +0000

    5+

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 import random
 
+class GuessingGame:
+    def __init__(self):
+        self.secret_number = random.randint(1, 99)
+        self.score = 10
+        self.is_running = True
 
-def main():
-    print("The game is started")
+    def check_even_odd(self):
+        if self.secret_number % 2 == 0:
+            print("The number is even")
+        else:
+            print("The number is odd ")
 
-    num = random.randint(1,99)
-    score = 10
-    while True:     
-        users_number = input("Guess the number: ")
-        if users_number == 'exit':
-            
-            break
-        elif users_number== 'is the number even':
-            if num % 2 ==0 :
-                print('The number is even')
+    def process_guess(self, user_input):
+        if user_input == 'exit':
+            self.is_running = False
+            return
+
+        if user_input == 'is the number even':
+            self.check_even_odd()
+            return
+
+        try:
+            guess = int(user_input)
+            if guess == self.secret_number:
+                print("You won")
+                print(f"Your score {self.score}")
+                self.is_running = False
             else:
-                print("the number is odd ")
-        elif num == users_number:
-            print("You won")
-            print(f'Your score {score}')
-        else: 
-            print("You lose")
-        score -= 1
-        
+                print("You lose")
+                self.score -= 1
+        except ValueError:
+            print("Invalid input. Please enter a number, 'is the number even' or 'exit'.")
 
-    
-
-    # TODO: Features 
-    # use loop
-    # take input for finishing the game
-    # take input for hint question 
-    # add scoring mechanism 
-
-    # TODO: Refactor
-    # extract classes
-    # use separate files
-
+    def start(self):
+        print("The game is started")
+        while self.is_running:
+            user_input = input("Guess the number: ").strip()
+            self.process_guess(user_input)
 
 
 if __name__ == "__main__":
-    main()
+    game = GuessingGame()
+    game.start()
